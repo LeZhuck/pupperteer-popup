9ff354e27b5b133be9b0d622a87f0f6574ef0edb
LeZhuck <101577633+LeZhuck@users.noreply.github.com>
2023-01-04 00:20:03 +0100

    feat : link for other websites

--- app/pages/index.js
+++ app/pages/index.js
@@ -15,7 +15,14 @@ export default function Home() {
     <div>
     <h1 className="text-2xl">Sujet de Stage Pupperteer</h1>
     <div className="flex justify-center items-center justify-items-center w-full mt-8">
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={myalert}> Click me </button>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded animate-bounce" onClick={myalert}> Click me </button>
+    </div>
+    <div className="flex justify-center items-center justify-items-center w-full mt-8">
+      You can also test on theses websites : 
+    </div>
+    <div className="flex justify-center items-center justify-items-center w-full mt-8">
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded animate-bounce"><a href = "https://demoqa.com/alerts"> Site 1</a> </button>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded animate-bounce ml-5"> <a href = "https://www.xul.fr/ecmascript/dialogues.php">Site 2 </a> </button>
     </div>
     </div>
   </div>
